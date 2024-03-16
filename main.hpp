38055f3c6d694b3f92e880378dbdf8c000537060
TealSmurf <jcoin707@gmail.com>
2024-03-16 00:13:22 -0700

    Copied over my read and write logic from the previous quiz to main.hpp

--- main.hpp
+++ main.hpp
@@ -1,3 +1,70 @@
-/***************************************************
- * Code your program here
- ***************************************************/
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+    ifstream ifs;
+
+    cout << "ID         " << "Name      " << "Department    " << "Salary" << endl;
+
+    int N, id, s1, total;
+    string eName, dName;
+    double avg;
+
+    ifs.open("employee.txt");
+    if (!ifs)
+    {
+        cout << "File Open Error\n";
+        exit(0);
+    }
+
+    if(ifs)
+    {
+        ifs >> N;
+        for(int i = 0; i < N; i++)
+        {
+            ifs >> id >> eName >> dName >> s1;
+            cout << id << "     " << eName << "     " << dName << "         " << s1 << endl;
+            total += s1;
+        }
+    }
+    avg = total/N;
+    cout << "               Total: " << total << "  Average: " << avg;
+
+    ifs.close();
+}
+
+int main()
+{
+    ofstream outfile("C:\\Users\\jcoin\\Downloads\\Comsc 110\\quiz-6-1-question-2-TealSmurf\\employee.txt");
+    
+    ofstream ofs;
+
+    int N;
+    int id, s1;
+    string eName, dName;
+
+    cout << "Enter the number of employees: ";
+    cin >> N;
+
+    ofs.open("employee.txt");
+    if (!ofs)
+    {
+        cout << "File Open Error\n";
+        exit(0);
+    }
+
+    if(ofs)
+    {
+        ofs << N << endl;
+        for(int i = 0; i < N; i++)
+        {
+            cout << "Enter their ID, employee name, department name, and salary: ";
+            cin >> id >> eName >> dName >> s1;
+            ofs << id << " " << eName << " " << dName << " " << s1 << endl;
+        }
+    }
+
+    ofs.close();
+}
